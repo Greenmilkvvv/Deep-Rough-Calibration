80f3d9c16bdd56f52d225b847f412f5a94b240a5
cdy’s PC <2764900676@qq.com>
2026-01-24 15:26:29 +0800

    update

--- NN_Training/rBergomi_resnet_pricer.py
+++ NN_Training/rBergomi_resnet_pricer.py
@@ -35,70 +35,11 @@ print(f"参数形状：{xx.shape}")
 yy = data[:, 4:]
 print(f"隐含波动率曲面形状：{yy.shape}")
 
-# 参数
 print(f"参数上界: {np.max(xx, axis=0)}")
 print(f"参数下界: {np.min(xx, axis=0)}")
 
+
 # %%
-# 画出 Implied Volatility
-from mpl_toolkits.mplot3d import Axes3D
-
-def ImpVol_surface_3d( 
-        xx_data: np.ndarray, 
-        yy_data: np.ndarray, 
-        K: np.array = np.array([0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5 ]), 
-        M: list = [0.1,0.3,0.6,0.9,1.2,1.5,1.8,2.0 ],
-        params_index: int = 10000, 
-        save_path = None
-): 
-    """
-    绘制隐含波动率曲面
-    
-    Args:
-        xx_data (np.ndarray): 参数
-        yy_data (np.ndarray): 隐含波动率曲面
-        K (np.array, optional): 行权价. Defaults to np.array([0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5 ]).
-        M (list, optional): 到期时间. Defaults to [0.1,0.3,0.6,0.9,1.2,1.5,1.8,2.0 ].
-        params_index (int, optional): 参数索引. Defaults to 10000.
-    """
-
-    model_params = xx_data[params_index,:]
-    
-    if yy_data.shape[1] != len(K) * len(M): return None
-
-    vol_surf_with_params = yy_data.reshape(-1, len(M), len(K))[params_index]
-
-    x_axis, y_axis = np.meshgrid(M, K, indexing='ij')
-
-    # Plot
-    fig = plt.figure(figsize=(10, 6))
-    ax = fig.add_subplot(111, projection='3d')
-
-    surf_to_plot = ax.plot_surface( 
-        x_axis, 
-        y_axis, 
-        vol_surf_with_params, 
-        cmap='viridis'
-    )
-    ax.set_xlabel('到期时间')
-    ax.set_ylabel('行权价')
-    ax.set_zlabel('隐含波动率')
-    ax.set_title(f"隐含波动率曲面 (参数: {model_params.tolist()})")
-    
-    # color bar
-    fig.colorbar(surf_to_plot, shrink=0.5, aspect=5)
-
-    if save_path:
-        plt.savefig(save_path, bbox_inches='tight', pad_inches=0.1, dpi=300)
-        
-        print(f"图像已保存至: {save_path}")
-    
-    plt.show()
-    
-    return 
-
-ImpVol_surface_3d(xx, yy, params_index=3999)
-
 
 # %%
 import torch
@@ -154,7 +95,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = torch.device(device)
 print(f"使用设备: {device}")
 
-# %%
+
 # 确定训练数据
 train_dataset = torch.utils.data.TensorDataset( 
     torch.from_numpy(x_train_transform).to(device=device),
@@ -181,7 +122,7 @@ data_loader = torch.utils.data.DataLoader(
 import sys
 sys.path.append(r"../") 
 
-from NN_Training.NN.nn import NN_pricing
+from NN_Training.NN.nn import NN_pricing, NN_pricing_ResNet
 from NN_Training.NN.training import train_model
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -195,7 +136,9 @@ hyperparams = {
     'block_layer_nums': 3
 }
 
-model = NN_pricing(hyperparams).to(device=device, dtype=torch.float64)
+# NN_pricing_ResNet
+## 加入残差网络改进的模型
+model = NN_pricing_ResNet(hyperparams).to(device=device, dtype=torch.float64)
 
 loss_MSE = nn.MSELoss()
 optim_Adam = torch.optim.Adam(model.parameters(), lr=0.0001)
@@ -213,32 +156,8 @@ tain_loss_lst, test_loss_lst = train_model(
 print(f"训练集损失: {tain_loss_lst[-1]}")
 print(f"测试集损失: {test_loss_lst[-1]}")
 
-torch.save(model.state_dict(), r"../Data/Models/nn_rBergomi.pth")
+torch.save(model.state_dict(), r"../Data/Models/nn_resnet_rBergomi.pth")
 
 print("模型已保存")
 
-# %%
-# # NN_pricing_ResNet
-# ## 加入残差网络改进的模型
-# model = NN_pricing_ResNet(hyperparams).to(device=device, dtype=torch.float64)
-
-# loss_MSE = nn.MSELoss()
-# optim_Adam = torch.optim.Adam(model.parameters(), lr=0.0001)
-
-# tain_loss_lst, test_loss_lst = train_model( 
-#     loss_MSE, 
-#     optim_Adam, 
-#     model, 
-#     data_loader, 
-#     train_data, 
-#     test_data, 
-#     epochs=10
-# )
-
-# print(f"训练集损失: {tain_loss_lst[-1]}")
-# print(f"测试集损失: {test_loss_lst[-1]}")
-
-# torch.save(model.state_dict(), r"../Data/Models/nn_resnet_rBergomi.pth")
-
-# print("模型已保存")
 
