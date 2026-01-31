d11f41ffa99a8c6d32837d335f5152c6c0d15718
cdy’s PC <2764900676@qq.com>
2026-01-31 21:23:08 +0800

    update gnn changes

--- NN_Training/rBergomi_gnn_pricer.py
+++ NN_Training/rBergomi_gnn_pricer.py
@@ -19,3 +19,35 @@ print(torch_geometric.__version__)
 
 
 # %%
+import gzip
+import numpy as np
+
+# 加载数据
+f = gzip.GzipFile( 
+    r"../Data/rBergomiTrainSet.txt.gz", 
+    "r"
+)
+data = np.load(f) 
+print(f"网格数据形状：{data.shape}")
+
+# 网格定义
+strikes=np.array([0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5 ])
+maturities=np.array([0.1,0.3,0.6,0.9,1.2,1.5,1.8,2.0 ])
+
+# xx: 参数
+## 前 4 列代表网格所对应的参数
+xx = data[:, :4]
+print(f"参数形状：{xx.shape}")
+
+# yy: 隐含波动率曲面 
+# 后 88 列表示隐含波动率曲面 8 * 11 = 88
+yy = data[:, 4:]
+print(f"隐含波动率曲面形状：{yy.shape}")
+
+# 参数
+print(f"参数上界: {np.max(xx, axis=0)}")
+print(f"参数下界: {np.min(xx, axis=0)}")
+
+
+# %%
+yy.reshape(-1, len(maturities), len(strikes))[10].shape
--- NN_Training/rBergomi_nn_pricer.py
+++ NN_Training/rBergomi_nn_pricer.py
@@ -3,7 +3,6 @@ from ast import mod
 import os
 
 import gzip
-import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
